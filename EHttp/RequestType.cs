c4df625f91b243a62f985bab578a36b0a29dd93e
stan e <yi.hang@live.com>
2017-12-14 22:17:52 +0800

    新增HTTP处理帮助类

---
+++ EHttp/RequestType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EHttp
+{
+    /// <summary>
+    /// 请求类型
+    /// </summary>
+    public enum RequestType
+    {
+        /// <summary>
+        /// Get
+        /// </summary>
+        GET = 0,
+        /// <summary>
+        /// Post
+        /// </summary>
+        POST = 1,
+    }
+}
